50ed404b38414a236f3e7d9e7aa6b03818d7f509
XinZheng2018 <xin.zheng@richmond.edu>
2020-10-20 22:18:14 +0800

    separate documents; incomplete; need preprocessing

--- QASystem.py
+++ QASystem.py
@@ -37,5 +37,93 @@ data structre:  a list of document for each qid , so it will be 50*20 dimension 
 
 """
 
+filename = 'training/topdocs/top_docs.0'
+def document_sep(filename):
+    rank = 0
+    doc = []
+    token_blocks = []
+
+    with open(filename, 'r', encoding='utf-8-sig') as f:
+        for line in f:
+            if "Rank" in line:
+                print("line contains rank: ")
+                print(line)
+                numbers = [int(word) for word in line.split() if word.isdigit()]
+                if rank != numbers[1]:
+                    print("length is")
+                    print(len(doc))
+                    print("numbers are:")
+                    print(numbers)
+                    print("rank is: ")
+                    print(rank)
+                    doc = []
+                    rank = numbers[1]
+
+            # document separated to lines
+            elif line.startswith("<P>"):
+                for line in f:
+                    if line.startswith("</P>"):
+                        break
+                    elif line.startswith("<"):
+                        continue
+                    else:
+                        for word in line.split():
+                            if not word in stop_words:
+                                doc.append(word)
+
+            # document stored as an entire text
+            # there are some other attributes that I did not cover
+            # in rank 4/5:
+            # <first> <second>, etc.
+            # in rank 21:
+            # <memo> <Caption> <LeadPara> <Section> needs to be cover
+            # also, I think the case where header goes to the next line need to be covered (not sure)
+            elif line.startswith("<HL>") or line.startswith("<HEAD>"):
+                for word in line.split():
+                    if not word in stop_words and (word != "</HL>" or word != "</HEAD>"):
+                        doc.append(word)
+            elif line.startswith("<AUTHOR>"):
+                for word in line.split():
+                    if not word in stop_words and word != "</AUTHOR>":
+                        doc.append(word)
+            elif line.startswith("<TEXT>"):
+                next_line = next(f)
+                # repeated code
+                # need to make it cleaner if possible
+                if next_line.startswith("<P>"):
+                    for line in f:
+                        if line.startswith("</TEXT>"):
+                            break
+                        elif line.startswith("</P>"):
+                            break
+                        elif line.startswith("<"):
+                            continue
+                        else:
+                            for word in line.split():
+                                if not word in stop_words:
+                                    doc.append(word)
+
+                else:
+                    for line in f:
+                        break_flag = False
+                        if line.startswith("</TEXT>"):
+                            break
+                        elif break_flag:
+                            break
+                        else:
+                            for word in line.split():
+                                if word == "</TEXT>":
+                                    break_flag = True
+                                    break
+                                else:
+                                    if not word in stop_words:
+                                        doc.append(word)
+                print(rank)
+                print(doc)
+            else:
+                continue
+
+document_sep(filename)
+
 
 
