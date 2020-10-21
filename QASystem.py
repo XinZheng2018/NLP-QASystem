2754fdbea1fadef9887c53ea195c7861306b70fe
XinZheng2018 <xin.zheng@richmond.edu>
2020-10-21 21:18:42 +0800

    add tags

--- QASystem.py
+++ QASystem.py
@@ -1,4 +1,8 @@
 import nltk
+import spacy
+import re
+
+
 from sklearn.preprocessing import Binarizer
 path = './training/qadata/questions.txt'
 stop_words = set(nltk.corpus.stopwords.words('english'))
@@ -11,18 +15,25 @@ def preprocessing_question(filename):
     @rtype:
     """
     tokenizer = nltk.RegexpTokenizer(r'\w+')
-    question_training = []
+    #question_training = []
+    question_training = {}
+    q_num = 0
     with open(filename, 'rt',encoding = 'utf-8-sig') as file:
         for line in file:
-            current_line = next(file)
+            if "Number" in line:
+                for word in line.split():
+                    if word.isdigit():
+                        q_num = int(word)
+            else:
+                # current_line = next(file)
             #if line[0]!='Number':
-            word_tokens = tokenizer.tokenize(current_line)
-            filter_sent = [word for word in word_tokens if not word in stop_words]
-            question_training.append(filter_sent)
-            next(file)
+                word_tokens = tokenizer.tokenize(line)
+                filter_sent = [word for word in word_tokens if not word in stop_words]
+                question_training[q_num] = filter_sent
+                next(file)
     return question_training
 
-# a = preprocessing(path)
+# a = preprocessing_question(path)
 # print(a)
 
 #todo: passage retrieval
@@ -42,7 +53,7 @@ new_added_stop_words = ['DATE','SECTION','P','LENGTH','HEADLINE','BYLINE','TEXT'
 for ele in new_added_stop_words:
     stop_words.add(ele)
 
-filename = './training/qadata/top_docs_test.0'
+filename = './training/topdocs/top_docs.0'
 #test_filename =
 def chunks(list, n):
     final_list =[]
@@ -110,7 +121,7 @@ def vectorize(candidate_passages,voc_list,question):
     for p in candidate_passages:
         temp = [1 if x in p else 0 for x in voc_list]
         bow.append(temp)
-    question_vectors = [1 if x in question else 0 for x in voc_list]
+    question_vectors = [1 if x in question[0] else 0 for x in voc_list]
         # transformer = Binarizer().fit_transform(p,voc_list)
         # print(transformer)
     return (bow,question_vectors)
@@ -130,167 +141,50 @@ def compute_similarity_find_max(bow,question_vectors,N):
         #     max_similariy=temp_sim
         #     backpointer = i
         # i+=1
-    copy_sim_list = copy.deepcopy(sim_list)
-    res = sorted(copy_sim_list, key=lambda x: x, reverse=True)[:N]
+    #copy_sim_list = copy.deepcopy(sim_list)
+    #res = sorted(copy_sim_list, key=lambda x: x, reverse=True)[:N]
     #same value ?"????
-    top_n_list = [bow(sim_list.index(x))for x in res]
-    return top_n_list
+    #top_n_list = [bow[sim_list.index(x)]for x in res]
+    top_n_indices = np.argsort(sim_list)[-N:]
+    return top_n_indices
+
+question = preprocessing_question(path)
+candidate_passage, voc_list = document_sep(filename)
+bow,question_vectors = vectorize(candidate_passage,voc_list,question)
+top_n_indices = compute_similarity_find_max(bow,question_vectors, 10)
+
+print(bow)
+print(top_n_indices)
 
 
+# use spacy to assign labels to each word in candidate passage
+sp = spacy.load('/opt/anaconda3/lib/python3.7/site-packages/en_core_web_sm')
 
+passage_with_label = []
 
-    # document separated to lines
-            # elif line.startswith("<P>"):
-            #     for line in f:
-            #         if line.startswith("</P>"):
-            #             break
-            #         elif line.startswith("<"):
-            #             continue
-            #         else:
-            #             for word in line.split():
-            #                 if not word in stop_words:
-            #                     doc.append(word)
+for i in top_n_indices:
+    passage = sp(candidate_passage[i])
+    entity = [ent for ent in passage.ents]
+    passage_with_label.append(entity)
 
-            # document stored as an entire text
-            # there are some other attributes that I did not cover
-            # in rank 4/5:
-            # <first> <second>, etc.
-            # in rank 21:
-            # <memo> <Caption> <LeadPara> <Section> needs to be cover
-            # also, I think the case where header goes to the next line need to be covered (not sure)
-            # code below is covered in the function
+for passage in passage_with_label:
+    for word in passage:
+        print(word.text + "-" + word.label_)
 
-
-<<<<<<< HEAD
-filename = 'training/topdocs/top_docs.0'
-def document_sep(filename):
-    rank = 0
-    doc = []
-    token_blocks = []
-
-    with open(filename, 'r', encoding='utf-8-sig') as f:
-        for line in f:
-            if "Rank" in line:
-                print("line contains rank: ")
-                print(line)
-                numbers = [int(word) for word in line.split() if word.isdigit()]
-                if rank != numbers[1]:
-                    doc = []
-                    rank = numbers[1]
-
-            elif line.startswith("<FIELDID>"):
-                # function to preprocess other tags
-                process_data()
-
-            # document separated to lines
-            elif line.startswith("<P>"):
-                for line in f:
-                    if line.startswith("</P>"):
-                        break
-                    elif line.startswith("<"):
-                        continue
-                    else:
-                        for word in line.split():
-                            if not word in stop_words:
-                                doc.append(word)
-
-            # document stored as an entire text
-            # there are some other attributes that I did not cover
-            # in rank 4/5:
-            # <first> <second>, etc.
-            # in rank 21:
-            # <memo> <Caption> <LeadPara> <Section> needs to be cover
-            # also, I think the case where header goes to the next line need to be covered (not sure)
-            # code below is covered in the function
-
-
-=======
->>>>>>> jing
-            # elif line.startswith("<HL>") or line.startswith("<HEAD>"):
-            #     for word in line.split():
-            #         if not word in stop_words and (word != "</HL>" or word != "</HEAD>"):
-            #             doc.append(word)
-            # elif line.startswith("<AUTHOR>"):
-            #     for word in line.split():
-            #         if not word in stop_words and word != "</AUTHOR>":
-            #             doc.append(word)
-<<<<<<< HEAD
-            elif line.startswith("<TEXT>"):
-                next_line = next(f)
-                # repeated code
-                # need to make it cleaner if possible
-                if next_line.startswith("<P>"):
-                    for line in f:
-                        if line.startswith("</TEXT>"):
-                            break
-                        elif line.startswith("</P>"):
-                            break
-                        elif line.startswith("<"):
-                            continue
-                        else:
-                            for word in line.split():
-                                if not word in stop_words:
-                                    doc.append(word)
-
-                else:
-                    for line in f:
-                        break_flag = False
-                        if line.startswith("</TEXT>"):
-                            break
-                        elif break_flag:
-                            break
-                        else:
-                            for word in line.split():
-                                if word == "</TEXT>":
-                                    break_flag = True
-                                    break
-                                else:
-                                    if not word in stop_words:
-                                        doc.append(word)
-                print(rank)
-                print(doc)
-            else:
-                continue
-
-document_sep(filename)
-
-=======
-            # elif line.startswith("<TEXT>"):
-            #     next_line = next(f)
-            #     # repeated code
-            #     # need to make it cleaner if possible
-            #     if next_line.startswith("<P>"):
-            #         for line in f:
-            #             if line.startswith("</TEXT>"):
-            #                 break
-            #             elif line.startswith("</P>"):
-            #                 break
-            #             elif line.startswith("<"):
-            #                 continue
-            #             else:
-            #                 for word in line.split():
-            #                     if not word in stop_words:
-            #                         doc.append(word)
-            #
-            #     else:
-            #         for line in f:
-            #             break_flag = False
-            #             if line.startswith("</TEXT>"):
-            #                 break
-            #             elif break_flag:
-            #                 break
-            #             else:
-            #                 for word in line.split():
-            #                     if word == "</TEXT>":
-            #                         break_flag = True
-            #                         break
-            #                     else:
-            #                         if not word in stop_words:
-            #                             doc.append(word)
-            #     print(rank)
-            #     print(doc)
-            # else:
-            #     continue
->>>>>>> jing
+answer_key = []
+# for query in question:
+#     q_key = re.search('Where', query, flags=re.IGNORECASE)
+#     if q_key:
+#         answer_key = []
+#         answer_key.append("PLACE")
+#     q_key = re.search('When', query, flags=re.IGNORECASE)
+#     if q_key:
+#         answer_key = []
+#         answer_key.append("DATE")
+#         answer_key.append("TIME")
+#     q_key = re.search('Who', query, flags=re.IGNORECASE)
+#     if q_key:
+#         answer_key = []
+#         answer_key.append("PERSON")
 
 
