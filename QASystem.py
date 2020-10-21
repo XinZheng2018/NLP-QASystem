c8a99a86fb7da2ba52108570abcca09094731f6e
irmadong <421332162@qq.com>
2020-10-21 01:06:39 -0400

    updated sim func

--- QASystem.py
+++ QASystem.py
@@ -1,8 +1,8 @@
 import nltk
-
+from sklearn.preprocessing import Binarizer
 path = './training/qadata/questions.txt'
 stop_words = set(nltk.corpus.stopwords.words('english'))
-def preprocessing(filename):
+def preprocessing_question(filename):
     """
     Preprocessing the question file
     @param filename:
@@ -57,8 +57,9 @@ def document_sep(filename,n=20):
     rank = 0
     doc = []
     candidate_passage = []
+    bow = set()
 
-    dict_document_tokenblocks ={}
+    #dict_document_tokenblocks ={}
     with open(filename, 'r', encoding='utf-8-sig') as f:
         #large_scale_tokenizer = nltk.RegexpTokenizer(r'\d+,?\d+|\s\w+|\w+\s')
         large_scale_tokenizer = nltk.RegexpTokenizer(r'\d+\smillion|\d+,?\d+|\w+')
@@ -70,37 +71,71 @@ def document_sep(filename,n=20):
                 if "Rank" in current_line:
                     print("line contains rank: ")
                     print(current_line)
-                    numbers = [int(word) for word in current_line.split() if word.isdigit()]
-                    if rank != numbers[1]:
-                        doc = []
-                        rank = numbers[1]
+                    # numbers = [int(word) for word in current_line.split() if word.isdigit()]
+                    # if rank != numbers[1]:
+                    #     doc = []
+                    #     rank = numbers[1]
                     #skip <doc>
                     next(f)
-                    current_line = next(f)
-                    current_docno = current_line.split()[1]
-                    dict_document_tokenblocks[current_docno] = []
+                    next(f)
+                    #current_line = next(f)
+                    #current_docno = current_line.split()[1]
+                    #dict_document_tokenblocks[current_docno] = []
                     current_line = next(f)
 
 
                     while "Rank" not in current_line:
                         current_tokens = large_scale_tokenizer.tokenize(current_line)
                         filter_token = [word for word in current_tokens if not word in stop_words]
+                        for ele in filter_token:
+                            bow.add(ele)
                         doc.extend(filter_token)
                         current_line=next(f)
 
 
-                    candidate_passage = chunks(doc,n)
-                    dict_document_tokenblocks[current_docno]=candidate_passage
+                    # candidate_passage = chunks(doc,n)
+                    # dict_document_tokenblocks[current_docno]=candidate_passage
         except StopIteration:
             print('EOF!')
             candidate_passage = chunks(doc, n)
-            dict_document_tokenblocks[current_docno] = candidate_passage
-            return dict_document_tokenblocks
+            voc_list = sorted(list(bow))
+            #dict_document_tokenblocks[current_docno] = candidate_passage
+            return (candidate_passage,voc_list)
 
-    return dict_document_tokenblocks
 
-result = document_sep(filename)
-print(result)
+#result = document_sep(filename)
+#print(result)
+def vectorize(candidate_passages,voc_list,question):
+    bow = []
+    for p in candidate_passages:
+        temp = [1 if x in p else 0 for x in voc_list]
+        bow.append(temp)
+    question_vectors = [1 if x in question else 0 for x in voc_list]
+        # transformer = Binarizer().fit_transform(p,voc_list)
+        # print(transformer)
+    return (bow,question_vectors)
+import math
+import numpy as np
+import copy
+def compute_similarity_find_max(bow,question_vectors,N):
+    max_similariy = -math.inf
+    #backpointer = 0
+    i = 0
+    sim_list = []
+    for vectors in bow:
+        temp_sim = np.dot(vectors,question_vectors)
+        sim_list.append(temp_sim)
+
+        # if temp_sim > max_similariy:
+        #     max_similariy=temp_sim
+        #     backpointer = i
+        # i+=1
+    copy_sim_list = copy.deepcopy(sim_list)
+    res = sorted(copy_sim_list, key=lambda x: x, reverse=True)[:N]
+    #same value ?"????
+    top_n_list = [bow(sim_list.index(x))for x in res]
+    return top_n_list
+
 
 
 
